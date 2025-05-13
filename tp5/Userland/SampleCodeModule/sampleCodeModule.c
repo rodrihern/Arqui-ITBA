891bf08a9e3e3839f4d1ff9d8950a38a5f0aca68
rodrihern <rohernandez003@gmail.com>
2025-05-13 20:13:37 -0300

    added compile.sh and tried to modify sample code but I cant see the changes after compiling

--- tp5/Userland/SampleCodeModule/sampleCodeModule.c
+++ tp5/Userland/SampleCodeModule/sampleCodeModule.c
@@ -8,7 +8,7 @@ static int var2 = 0;
 
 int main() {
 	//All the following code may be removed 
-	*v = 'X';
+	*v = 'R';
 	*(v+1) = 0x74;
 
 	//Test if BSS is properly set up
---
+++ tp5/compile.sh
@@ -0,0 +1,4 @@
+docker exec -it tp5 make -C /root/Toolchain clean
+docker exec -it tp5 make -C /root/Toolchain all
+docker exec -it tp5 make -C /root clean
+docker exec -it tp5 make -C /root all
